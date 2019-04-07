727f3a4cc1883cc0b2b57caeec98c4dea6b9e73b
ZHUWENLONG <1573511441@qq.com>
2019-04-07 21:28:45 +0800

    search

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
+public searchValue:string=''
+public catchList:Array<any>=[
+  {
+    title:'默认值',
+    checked:false,
+    isDel:false
+  }
+]
   constructor() { }
 
   ngOnInit() {
+  }
+  doSearch(){
+    console.log(this.searchValue)
+    let obj:object={
+      title:this.searchValue,
+      checked:false,
+      isDel:false,
+      id:new Date().getTime()
+    }
+    this.catchList.push(obj)
+    console.log(this.catchList)
+    this.searchValue = ''
+  }
+  delValue(id:number){
+    console.log(id)
+
   }
 
 }
